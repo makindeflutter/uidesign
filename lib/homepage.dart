3615d9fc467b9b7ccc2b73e81a789bee8d05e30c
makindeflutter <proffakiniyinka@gmail.com>
2022-12-16 10:00:40 +0100

    main

---
+++ lib/homepage.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class FavouriteCity extends StatefulWidget {
+  const FavouriteCity({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FavouriteCity();
+  }
+}
+
+class _FavouriteCity extends State<FavouriteCity> {
+  String nameCity = "";
+  final _currency = ['Naira', 'DOllar', 'Euros'];
+  var _currentItemSelected = 'Naira';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('stateful widget'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(30.0),
+        child: Column(children: <Widget>[
+          TextField(
+            onSubmitted: (String userinput) {
+              setState(() {
+                nameCity = userinput;
+              });
+            },
+          ),
+          DropdownButton(
+            items: _currency.map((String dropDownStringItem) {
+              return DropdownMenuItem<String>(
+                value: dropDownStringItem,
+                child: Text(dropDownStringItem),
+              );
+            }).toList(),
+            onChanged: (newSelectedValue) {
+              setState(() {
+                _currentItemSelected = newSelectedValue!;
+              });
+            },
+            value: _currentItemSelected,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              nameCity,
+              style: const TextStyle(fontSize: 30.00),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,67 @@
 import 'package:flutter/material.dart';
 
+import 'homepage.dart';
+
 main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     title: 'UI',
     home: FavouriteCity(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-class FavouriteCity extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _FavouriteCity();
-  }
-}
+// class FavouriteCity extends StatefulWidget {
+//   const FavouriteCity({super.key});
 
-class _FavouriteCity extends State<FavouriteCity> {
-  String nameCity = "";
-  var _currency = ['Naira', 'DOllar', 'Euros'];
-  var _currentItemSelected = 'Naira';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('stateful widget'),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(30.0),
-        child: Column(children: <Widget>[
-          TextField(
-            onSubmitted: (String userinput) {
-              setState(() {
-                nameCity = userinput;
-              });
-            },
-          ),
-          DropdownButton(
-            items: _currency.map((String dropDownStringItem) {
-              return DropdownMenuItem<String>(
-                value: dropDownStringItem,
-                child: Text(dropDownStringItem),
-              );
-            }).toList(),
-            onChanged: (newSelectedValue) {
-              setState(() {
-                _currentItemSelected = newSelectedValue!;
-              });
-            },
-            value: _currentItemSelected,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Text(
-              nameCity,
-              style: TextStyle(fontSize: 30.00),
-            ),
-          ),
-        ]),
-      ),
-    );
-  }
-}
+//   @override
+//   State<StatefulWidget> createState() {
+//     return _FavouriteCity();
+//   }
+// }
+
+// class _FavouriteCity extends State<FavouriteCity> {
+//   String nameCity = "";
+//   final _currency = ['Naira', 'DOllar', 'Euros'];
+//   var _currentItemSelected = 'Naira';
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('stateful widget'),
+//       ),
+//       body: Container(
+//         margin: const EdgeInsets.all(30.0),
+//         child: Column(children: <Widget>[
+//           TextField(
+//             onSubmitted: (String userinput) {
+//               setState(() {
+//                 nameCity = userinput;
+//               });
+//             },
+//           ),
+//           DropdownButton(
+//             items: _currency.map((String dropDownStringItem) {
+//               return DropdownMenuItem<String>(
+//                 value: dropDownStringItem,
+//                 child: Text(dropDownStringItem),
+//               );
+//             }).toList(),
+//             onChanged: (newSelectedValue) {
+//               setState(() {
+//                 _currentItemSelected = newSelectedValue!;
+//               });
+//             },
+//             value: _currentItemSelected,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(30.0),
+//             child: Text(
+//               nameCity,
+//               style: const TextStyle(fontSize: 30.00),
+//             ),
+//           ),
+//         ]),
+//       ),
+//     );
+//   }
+// }
